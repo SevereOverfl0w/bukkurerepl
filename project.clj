f1d57ad4bac37787764857b5bd19e1829ecd228e
SevereOverfl0w <overfl0w@narna.co>
2015-07-21 13:44:04 +0100

    add nrepl dep.

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject repl "0.1.0-SNAPSHOT"
   :description "A bukkure plugin to..."
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [bukkure "0.2.1-SNAPSHOT"]]
+                 [bukkure "0.2.1-SNAPSHOT"]
+                 [org.clojure/tools.nrepl "0.2.10"]]
   :java-source-paths ["javasrc"])
